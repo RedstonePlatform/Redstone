8a8eea63a4cf436a022399ba2fce3c7208c9e3f2
spartacrypt <spartaccrypt@protonmail.com>
2018-06-26 08:44:37 +0100

    add dns node and change back to pow

---
+++ src/Redstone/Programs/Redstone.RedstoneFullNodeDnsD/Program.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NBitcoin;
+using NBitcoin.Protocol;
+using Redstone.Features.Api;
+using Stratis.Bitcoin;
+using Stratis.Bitcoin.Builder;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.Features.BlockStore;
+using Stratis.Bitcoin.Features.Consensus;
+using Stratis.Bitcoin.Features.Dns;
+using Stratis.Bitcoin.Features.MemoryPool;
+using Stratis.Bitcoin.Features.Miner;
+using Stratis.Bitcoin.Features.RPC;
+using Stratis.Bitcoin.Features.Wallet;
+using Stratis.Bitcoin.Utilities;
+
+namespace Redstone.RedstoneFullNodeDnsD
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+
+        public static async Task MainAsync(string[] args)
+        {
+            try
+            {
+                Network network = args.Contains("-testnet") ? Network.RedstoneTest : Network.RedstoneMain;
+                NodeSettings nodeSettings = new NodeSettings(network, ProtocolVersion.ALT_PROTOCOL_VERSION, args: args);
+
+                var dnsSettings = new DnsSettings(nodeSettings);
+
+                if (string.IsNullOrWhiteSpace(dnsSettings.DnsHostName) || string.IsNullOrWhiteSpace(dnsSettings.DnsNameServer) || string.IsNullOrWhiteSpace(dnsSettings.DnsMailBox))
+                    throw new ConfigurationException("When running as a DNS Seed service, the -dnshostname, -dnsnameserver and -dnsmailbox arguments must be specified on the command line.");
+
+                // Run as a full node with DNS or just a DNS service?
+                IFullNode node;
+                if (dnsSettings.DnsFullNode)
+                {
+                    // Build the Dns full node.
+                    node = new FullNodeBuilder()
+                        .UseNodeSettings(nodeSettings)
+                        .UseBlockStore()
+                        //.UsePosConsensus()
+                        .UsePowConsensus()
+                        .UseMempool()
+                        .UseWallet()
+                        //.AddPowPosMining()
+                        .AddMining()
+                        .UseApi()
+                        .AddRPC()
+                        .UseDns()
+                        .Build();
+                }
+                else
+                {
+                    // Build the Dns node.
+                    node = new FullNodeBuilder()
+                        .UseNodeSettings(nodeSettings)
+                        //.UsePosConsensus()
+                        .UsePowConsensus()
+                        .UseApi()
+                        .AddRPC()
+                        .UseDns()
+                        .Build();
+                }
+
+                if (node != null)
+                    await node.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem initializing the node. Details: '{0}'", ex.Message);
+            }
+        }
+    }
+}
