5600fda8dfd521c296038e5d8b3a6cfb58f6c5d6
dirty-harry <41760565+dirty-harry@users.noreply.github.com>
2018-10-10 12:33:14 +0100

    Revert "Bug/issue loading existing database"

--- src/Stratis.Bitcoin.Features.Consensus/StakeChainStore.cs
+++ src/Stratis.Bitcoin.Features.Consensus/StakeChainStore.cs
@@ -49,9 +49,8 @@ namespace Stratis.Bitcoin.Features.Consensus
 
         public async Task LoadAsync()
         {
-            /// TODO temp fix for now as per Sondreb code; perm fix needed
-			//uint256 hash = await this.dBreezeCoinView.GetTipHashAsync().ConfigureAwait(false);
-            ChainedHeader next = this.chain.Tip; // this.chain.GetBlock(hash);
+            uint256 hash = await this.dBreezeCoinView.GetTipHashAsync().ConfigureAwait(false);
+            ChainedHeader next = this.chain.GetBlock(hash);
             var load = new List<StakeItem>();
 
             while (next != this.chain.Genesis)
