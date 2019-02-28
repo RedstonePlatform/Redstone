efff8fbb6dde0d5b5bb254c1dc303404ce2e584f
dirty-harry <41760565+dirty-harry@users.noreply.github.com>
2019-02-28 22:56:51 +0000

    Fix explorer node install

--- Scripts/Node-Installer/install_redstone_explorer_node.sh
+++ Scripts/Node-Installer/install_redstone_explorer_node.sh
@@ -268,11 +268,12 @@ installMongodDB() {
 	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 &>> ${SCRIPT_LOGFILE}
 	if [[ -r /etc/os-release ]]; then
 	. /etc/os-release
-	if [[ "${VERSION_ID}" = "16.04" ]]; then
-		echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-	fi
-	if [[ "${VERSION_ID}" = "18.04" ]]; then
-		echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+		if [[ "${VERSION_ID}" = "16.04" ]]; then
+			echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+		fi
+		if [[ "${VERSION_ID}" = "18.04" ]]; then
+			echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+		fi
 	fi
 	sudo apt-get update &>> ${SCRIPT_LOGFILE}
 	sudo apt-get install -y mongodb-org &>> ${SCRIPT_LOGFILE}
