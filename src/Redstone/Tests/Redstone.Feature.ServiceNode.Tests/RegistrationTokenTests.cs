08e91c1ea9a4ebb3b3a93b70fea9d029ad79a3be
spartacrypt <spartacrypt@protonmail.com>
2019-06-05 18:50:12 +0100

    #snrefactor: unit tests

--- src/Redstone/Tests/Redstone.Feature.ServiceNode.Tests/RegistrationTokenTests.cs
+++ src/Redstone/Tests/Redstone.Feature.ServiceNode.Tests/RegistrationTokenTests.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Text;
 using NBitcoin;
 using Redstone.Core.Networks;
 using Redstone.Features.ServiceNode;
@@ -33,6 +34,55 @@ namespace Redstone.Feature.ServiceNode.Tests
             token.EcdsaSignature = cryptoUtils.SignDataECDSA(token.GetHeaderBytes().ToArray());
 
             Assert.True(token.Validate(RedstoneNetworks.Main));
+        }
+
+        
+        [Fact]
+        public void CheckSignatureOfRegistrationToken()
+        {
+            var rsa = new RsaKey();
+            var ecdsa = new Key().GetBitcoinSecret(RedstoneNetworks.Main);
+
+            var token = new RegistrationToken(1,
+                "175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W",
+                IPAddress.Parse("172.16.1.10"),
+                IPAddress.Parse("2001:0db8:85a3:0000:1234:8a2e:0370:7334"),
+                "5678901234ABCDEF",
+                "",
+                16174,
+                ecdsa.PubKey);
+
+            // Only the 'header' portion of the registration token gets signed, minus the length bytes
+            var message = token.GetHeaderBytes();
+
+            var cryptoUtils = new CryptoUtils(rsa, ecdsa);
+            token.RsaSignature = cryptoUtils.SignDataRSA(message.ToArray());
+            token.EcdsaSignature = cryptoUtils.SignDataECDSA(message.ToArray());
+
+            var signature = cryptoUtils.SignDataECDSA(message.ToArray());
+            Assert.True(CryptoUtils.VerifySignatureECDSA(message.ToArray(), ecdsa.PubKey, Encoding.UTF8.GetString(signature)));
+            Assert.True(token.VerifySignatures());
+        }
+
+        [Fact]
+        public void CanVerifySignature()
+        {
+            var rsa = new RsaKey();
+            var ecdsa = new Key().GetBitcoinSecret(RedstoneNetworks.Main);
+
+            var token = new RegistrationToken(1,
+                "175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W",
+                IPAddress.Parse("172.16.1.10"),
+                IPAddress.Parse("2001:0db8:85a3:0000:1234:8a2e:0370:7334"),
+                "5678901234ABCDEF",
+                "",
+                16174,
+                ecdsa.PubKey);
+
+            var cryptoUtils = new CryptoUtils(rsa, ecdsa);
+            token.EcdsaSignature = cryptoUtils.SignDataECDSA(token.GetHeaderBytes().ToArray());
+
+            Assert.True(token.VerifySignatures());
         }
     }
 }
